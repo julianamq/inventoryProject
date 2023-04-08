7ef683b510bd1bb4fb00d1e938415f7083d103e6
Juliana <julianamq20@gmail.com>
2023-04-08 16:46:52 -0300

    iniciando project

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    create_product = Product(
+        1,
+        "Borracha",
+        "Papelaria Solar",
+        "2021-07-04",
+        "2029-02-09",
+        "FR48",
+        "Ao abrigo de luz solar",
+    )
+    assert create_product.id == 1
+    assert create_product.nome_do_produto == "Borracha"
+    assert create_product.nome_da_empresa == "Papelaria Solar"
+    assert create_product.data_de_fabricacao == "2021-07-04"
+    assert create_product.data_de_validade == "2029-02-09"
+    assert create_product.numero_de_serie == "FR48"
+    assert (
+        create_product.instrucoes_de_armazenamento == "Ao abrigo de luz solar"
+    )

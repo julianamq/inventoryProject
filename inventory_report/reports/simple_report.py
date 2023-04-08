c3bc074436f7575e021f10bc21ffbfef0fbb4819
Juliana <julianamq20@gmail.com>
2023-04-08 17:01:55 -0300

    feat: Simple Report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,26 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    # https://www.programiz.com/python-programming/methods/built-in/staticmethod
+    def generate(products):
+        min_date = min(product["data_de_fabricacao"] for product in products)
+        max_date = min(
+            (
+                product["data_de_validade"]
+                for product in products
+                if product["data_de_validade"]
+                > datetime.now().strftime("%Y-%m-%d")
+            ),
+            default=None,
+        )
+        most_common_company = Counter(
+            product["nome_da_empresa"] for product in products
+        ).most_common(1)[0][0]
+        return (
+            f"Data de fabricação mais antiga: {min_date}\n"
+            f"Data de validade mais próxima: {max_date}\n"
+            f"Empresa com mais produtos: {most_common_company}"
+        )

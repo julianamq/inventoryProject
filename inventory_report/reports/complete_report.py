7a03ca7f72b26fc417697215babe4ade6adc3b63
Juliana <julianamq20@gmail.com>
2023-04-08 17:26:53 -0300

    tentativa : 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,12 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, list):
+        result = super().generate(list)
+        products_stocked_by_company = cls.get_company(list)
+        result += "\nProdutos estocados por empresa:\n"
+        for c, q in products_stocked_by_company.items():
+            result += f"- {c}: {q}\n"
+        return result

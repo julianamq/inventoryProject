db4a9aec3cbe415466db3c0320648eeaa5e4d6a3
Juliana <julianamq20@gmail.com>
2023-04-11 15:57:20 -0300

    lint

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -7,33 +7,30 @@ from inventory_report.reports.simple_report import SimpleReport
 class Inventory:
     @classmethod
     def import_data(cls, path: str, relatory: str):
-        with open(path, encoding='utf-8') as file:
-            products = csv.DictReader(file, delimiter=',', quotechar='"')
+        with open(path, encoding="utf-8") as file:
+            products = csv.DictReader(file, delimiter=",", quotechar='"')
             read_products = []
             for product in products:
                 read_products.append(product)
-            read_products = list(read_products)
+                read_products = list(read_products)
+                if relatory == "simples":
+                    return SimpleReport.generate(read_products)
+                elif relatory == "completo":
+                    return CompleteReport.generate(read_products)
+                raise ValueError("Erro ao ler o arquivo de relatório")
 
-        if relatory == 'simples':
-            return SimpleReport.generate(read_products)
-        elif relatory == 'completo':
-            return CompleteReport.generate(read_products)
-        raise ValueError(
-            'Erro ao ler o arquivo de relatório')
-    
     @classmethod
     def read_archives(cls, path: str):
-        if path.endswith('csv'):
-            with open(path, encoding='utf-8') as file:
-                aquivo_csv = csv.DictReader(file, delimiter=',', quotechar='"')
+        if path.endswith("csv"):
+            with open(path, encoding="utf-8") as file:
+                aquivo_csv = csv.DictReader(file, delimiter=",", quotechar='"')
                 read_products = []
                 for product in aquivo_csv:
                     read_products.append(product)
-                read_products = list(read_products)
-                return read_products
-        elif path.endswith('json'):
+                    read_products = list(read_products)
+                    return read_products
+        elif path.endswith("json"):
             with open(path) as file:
                 content = file.read()
                 read = json.loads(content)
                 return read
-            

d5126d707c46fc1afeb27fbe195f29bc501b20a1
Juliana <julianamq20@gmail.com>
2023-04-12 17:00:17 -0300

    lista add e requisito refatorado

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -3,19 +3,17 @@ from .importer import Importer
 
 
 class XmlImporter(Importer):
-    @staticmethod
-    def import_data(file_name: str) -> list:
-        if not file_name.endswith('.xml'):
+    @classmethod
+    def import_data(cls, path):
+        if ".xml" not in path: # se houver erro será levantado
             raise ValueError("Arquivo inválido")
-
-        tree = ET.parse(file_name)
-        root = tree.getroot()
-
-        data = []
-        for item in root.findall('item'):
-            product = {}
+        types = []
+        with open(path) as file:
+            tree = ET.parse(file)
+            item = tree.getroot()
             for child in item:
-                product[child.tag] = child.text
-            data.append(product)
-
-        return data
+                item = {}
+                for items in child:
+                    item[items.tag] = items.text
+                types.append(item)
+        return types

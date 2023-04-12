08a1ffea04301af47ca35543be4de1dbb739c644
Juliana <julianamq20@gmail.com>
2023-04-12 15:52:49 -0300

    mentoria : Carlos

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -7,12 +7,11 @@ import json
 class Inventory:
     @classmethod
     def import_data(cls, path: str, relatory: str):
-        with open(path, encoding="utf-8") as file:
-            products = csv.DictReader(file, delimiter=",", quotechar='"')
-            list_products = []
-            for product in products:
-                list_products.append(product)
-            list_products = list(list_products)
+        list_products = []
+        if path.endswith("csv"):
+            list_products = Inventory.read_archives(path)                           
+        elif path.endswith("json"):
+            list_products = Inventory.read_json(path)
 
         if relatory == "simples":
             return SimpleReport.generate(list_products)
@@ -20,15 +19,13 @@ class Inventory:
             return CompleteReport.generate(list_products)
         raise ValueError("Erro ao ler o arquivo de relatório")
 
-    @classmethod
     def read_archives(path):
-        with open(path, "read") as file:
+        with open(path, "r") as file:
             arq_csv = csv.DictReader(file, delimiter=",", quotechar='"')
             read_products = []
             for product in arq_csv:
                 read_products.append(product)
-                read_products = list(read_products)
-        return read_products
+            return read_products
 
     def read_json(path):
         with open(path) as file:
